6a1a8214fe8e1fb8756c51ecfe45fdfbab48e813
Julian Zimmermann <julian@jzit.de>
2020-12-11 17:00:36 +0100

    moved blynk install and start from dockerfile to entrypoint

--- Dockerfile
+++ Dockerfile
@@ -18,12 +18,8 @@ RUN wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -
 COPY ./config/server.properties.template /etc/blynk/config/server.properties.template
 COPY ./docker-entrypoint/blynk.sh /usr/local/bin/docker-entrypoint.sh
 
-RUN wget https://github.com/blynkkk/blynk-server/releases/download/v0.41.13/server-0.41.13-java8.jar \
-    && mkdir -p /blynk/data \
-    && mkdir -p /blynk/config \
-    && mkdir -p /blynk/log \
-    && chmod +x /usr/local/bin/docker-entrypoint.sh
+RUN chmod +x /usr/local/bin/docker-entrypoint.sh
 
 ENTRYPOINT ["docker-entrypoint.sh"]
 
-CMD java -jar server-0.41.13-java8.jar -dataFolder /blynk/data -serverConfig /blynk/config/server.properties
+CMD tail -f /dev/null
--- docker-entrypoint/blynk.sh
+++ docker-entrypoint/blynk.sh
@@ -3,6 +3,35 @@ set -e
 
 export ESC="$"
 
+if [ ! -d "$BLYNK_FOLDER" ]; then
+    mkdir -p "$BLYNK_FOLDER"
+fi
+
+if [ ! -d "$BLYNK_DATA_FOLDER" ]; then
+    mkdir -p "$BLYNK_DATA_FOLDER"
+fi
+
+if [ ! -d "$BLYNK_CONFIG_FOLDER" ]; then
+    mkdir -p "$BLYNK_CONFIG_FOLDER"
+fi
+
+if [ ! -d "$BLYNK_LOG_FOLDER" ]; then
+    mkdir -p "$BLYNK_LOG_FOLDER"
+fi
+
+blynkFileName=${BLYNK_DOWNLOAD_URL##*/}
+
+if [ ! -f "$BLYNK_FOLDER"/"$blynkFileName" ]; then
+    cd "$BLYNK_FOLDER"
+    if ! wget "$BLYNK_DOWNLOAD_URL"; then
+        echo "ERROR: can't get $BLYNK_DOWNLOAD_URL" >&2
+        exit 1
+    fi
+    cd ..
+fi
+
 envsubst < /etc/blynk/config/server.properties.template > "$BLYNK_CONFIG_FOLDER"/server.properties
+
+nohup java -jar "$BLYNK_FOLDER"/"$blynkFileName" -dataFolder "$BLYNK_DATA_FOLDER" -serverConfig "$BLYNK_CONFIG_FOLDER"/server.properties
 
 exec "$@"
